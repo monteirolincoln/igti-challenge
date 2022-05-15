77294605549616a3f61d22500f137b24c675c00f
monteirolincoln <monteiroawseducate@gmail.com>
2022-05-15 09:29:37 -0300

    first commit

---
+++ infrastructure/variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+  default = "us-east-2"
+}
+
+variable "lambda_function_name" {
+  default = "IGTIexecutaEMRaovivo"
+}
+
+variable "key_pair_name" {
+  default = "lin_igti_key_pair"
+}
+
+variable "vpc_id" {
+  default = "vpc-d724b4bc"
+}
