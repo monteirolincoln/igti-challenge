381ea8388f1e93bb02a4390ec943342d4d9f86c3
monteirolincoln <monteiroawseducate@gmail.com>
2022-05-15 09:56:19 -0300

    new commit

---
+++ infrastructure/s3.tf
@@ -0,0 +1,36 @@
+resource "aws_s3_bucket" "dl" {
+  bucket = "datalake-lin-igti-edc-tf"
+  acl    = "private"
+
+  tags = {
+    IES   = "IGTI",
+    CURSO = "EDC"
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+
+resource "aws_s3_bucket" "stream" {
+  bucket = "igti-lin-streaming-bucket"
+  acl    = "private"
+
+  tags = {
+    IES   = "IGTI",
+    CURSO = "EDC"
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
