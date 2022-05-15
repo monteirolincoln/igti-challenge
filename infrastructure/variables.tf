02a877c8b58e259ab358c49599672ad782933f92
monteirolincoln <monteiroawseducate@gmail.com>
2022-05-15 11:08:36 -0300

    add variables lambda function

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -6,10 +6,9 @@ variable "key_pair_name" {
   default = "lin_igti_key_pair"
 }
 
-variable "airflow_subnet_id" {
-  default="subnet-0a7cd403037f4f111"
+variable "lambda_function_name" {
+  default = "IGTIexecutaEMRaovivo"
 }
-
 variable "vpc_id" {
   default = "vpc-05599c94b48bd9e24"
 }
