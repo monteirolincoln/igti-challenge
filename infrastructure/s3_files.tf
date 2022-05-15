e4d305e62fdf4eda0382e49adc5a6a65d5d695a6
monteirolincoln <monteiroawseducate@gmail.com>
2022-05-15 11:45:59 -0300

    add centro oeste

--- infrastructure/s3_files.tf
+++ infrastructure/s3_files.tf
@@ -1,9 +1,9 @@
 resource "aws_s3_bucket_object" "upload" {
   bucket = aws_s3_bucket.raw-data.id
-  key    = "data/rais_vinc_pub_norte.csv"
+  key    = "data/rais_vinc_pub_centro_oeste.csv"
   acl    = "private"
-  source = "../data/RAIS_VINC_PUB_NORTE.txt"
-  etag   = filemd5("../data/RAIS_VINC_PUB_NORTE.txt")
+  source = "../data/RAIS_VINC_PUB_CENTRO_OESTE.txt"
+  etag   = filemd5("../data/RAIS_VINC_PUB_CENTRO_OESTE.txt")
 }
 
 
