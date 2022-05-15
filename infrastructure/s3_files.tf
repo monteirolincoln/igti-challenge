3c41f5110cad2c485bd8f312a56e67798746df2a
monteirolincoln <monteiroawseducate@gmail.com>
2022-05-15 11:18:04 -0300

    add file

--- infrastructure/s3_files.tf
+++ infrastructure/s3_files.tf
@@ -2,8 +2,8 @@ resource "aws_s3_bucket_object" "upload" {
   bucket = aws_s3_bucket.raw-data.id
   key    = "data/rais_vinc_pub_norte.csv"
   acl    = "private"
-  source = "data/RAIS_VINC_PUB_NORTE.txt"
-  etag   = filemd5("data/RAIS_VINC_PUB_NORTE.txt")
+  source = "../data/RAIS_VINC_PUB_NORTE.txt"
+  etag   = filemd5("../data/RAIS_VINC_PUB_NORTE.txt")
 }
 
 
